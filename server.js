15c9ba720617d54515f45fc0bee76171c80d0715
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2017-01-15 00:00:14 -0200

    Update server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express    = require('express');   // call express
 var app        = express();            // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-mongoose.connect( 'mongodb://marcosmcb:Markos2525-@ds111529.mlab.com:11529/pagesdb' );   // connect to our database
+mongoose.connect( 'mongodb://<dbuser>:<dbpassword>@ds111529.mlab.com:11529/pagesdb' );   // connect to our database
 
 var Page       = require('./app/models/page');
 
